b1e16aecf12abdf6e8b7ee1d230bf678339ff396
Edd-war <digiespirit.1807@gmail.com>
2021-09-16 16:52:45 -0500

    Future Builder
    Se obtienen los valores del json para colocarlos en la lista.
    Aquí tuve que implementar la línea 46 por que del modo que estaba en el curso, así no compilaba

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
-import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:flutter/material.dart';
+
+import 'package:componentes/src/providers/menu_provider.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,19 +17,43 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-    return ListView(
-      children: _listaItems(),
+    // print(menuProvider.opciones);
+    //CON ESTE MÉTODO LA APLICACIÓN PUEDE CONGELARSE
+    // menuProvider.cargarData().then((opciones) {
+    //   print('Lista');
+    //   print(opciones);
+    // });
+    // return ListView(
+    //   children: _listaItems(),
+    // );
+
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      // initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+
+        return ListView(
+            children: _listaItems(snapshot.data)
+        );
+      }
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-    ];
+  List<Widget> _listaItems(List<dynamic>? data) {
+    final List<Widget> opciones = [];
+    if(data!=null)
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        title: Text(opt['texto']),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.red),
+        onTap: (){
+
+        },
+      );
+      opciones..add(widgetTemp)
+            ..add(Divider());
+    });
+    return opciones;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -7,14 +7,14 @@ class _MenuProvider{
   List<dynamic> opciones = [];
 
   _MenuProvider(){
-    cargarData();
+    // cargarData();
   }
 
   Future<List<dynamic>> cargarData() async{
     final resp = await rootBundle.loadString('data/menu_opts.json');
 
     Map dataMap = json.decode(resp);
-    print(dataMap['rutas']);
+    // print(dataMap['rutas']);
     opciones = dataMap['rutas'];
 
     return opciones;
