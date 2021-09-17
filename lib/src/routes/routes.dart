8fb9186e3215b7b06a7e08d49a053e54acd344e7
Edd-war <digiespirit.1807@gmail.com>
2021-09-17 00:20:12 -0500

    Container y AnimatedContainer
    Al crear un nuevo recurso de página, se tiene que editar el json para registrar en la lista las nuevas propiedades de tal recurso, en el caso la nueva página es llamada animated_controller.
    Tambien se crea la ruta a la página y en las utilidades de los íconos se agrega el controlador de este.
    En el nuevo recurso se trabaja sobre la animación por segundo de un contenedor con alto y ancho que cambia de medias, color y bordes aleatoriamente.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/animated_container.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/card_page.dart';
@@ -6,10 +7,11 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 
 Map <String, WidgetBuilder> getApplicationRoutes() => <String, WidgetBuilder>{
-    '/'        : (BuildContext context) => HomePage(),
-    'alert'    : (BuildContext context) => AlertPage(),
-    'avatar'   : (BuildContext context) => AvatarPage(),
-    'card'     : (BuildContext context) => CardPage(),
+    '/'                     : (BuildContext context) => HomePage(),
+    'alert'                 : (BuildContext context) => AlertPage(),
+    'avatar'                : (BuildContext context) => AvatarPage(),
+    'card'                  : (BuildContext context) => CardPage(),
+    'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
   };
 
 
