7766b7358ab81d9f4a008ebfee70b920c2768b41
Edd-war <digiespirit.1807@gmail.com>
2021-09-16 22:36:50 -0500

    Widget - Circle Avatar
    Se agrega el ícono de avatar en parte superior de dos formas y en el body una imagen más grande de esa persona.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://www.biografiasyvidas.com/biografia/t/fotos/turing.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('AT'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://media.globalcitizen.org/thumbnails/1d/ff/1dff4fc6-18f5-4416-a5c3-a1fb5d73bbc5/alan_turing.jpg__1600x900_q85_crop_subsampling-2.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
