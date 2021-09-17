b6ea71857c3caf24e2cf9cde3fb4967bef63d066
Edd-war <digiespirit.1807@gmail.com>
2021-09-17 00:51:41 -0500

    inputFields
    Creación de nuevo recurso probando la caja de texto con bonito diseño que cuenta las letras y capitaliza las iniciales de cada sentencia.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/card_page.dart';
@@ -12,6 +13,7 @@ Map <String, WidgetBuilder> getApplicationRoutes() => <String, WidgetBuilder>{
     'avatar'                : (BuildContext context) => AvatarPage(),
     'card'                  : (BuildContext context) => CardPage(),
     'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
+    'inputs'                : (BuildContext context) => InputPage(),
   };
 
 
