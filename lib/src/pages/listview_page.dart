7cd0e13226751b332dd2e7a8ba2ff6e9682bb57d
Edd-war <digiespirit.1807@gmail.com>
2021-09-18 00:54:20 -0500

    InfiniteScroll
    Se agrega un controlador del scroll capaz de ser reactivo para alertar cuando se llegue al máximo hasta abajo, pueda la app cargar 10 imágenes más del sitio.

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -9,7 +9,23 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _agregar10();
+    _scrollController.addListener(() {
+      // print('SCROL!');
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        _agregar10();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +39,7 @@ class _ListaPageState extends State<ListaPage> {
 
   Widget _crearLista(){
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _listaNumeros.length,
       itemBuilder: (BuildContext context, int index){
         final imagen = _listaNumeros[index];
@@ -33,5 +50,15 @@ class _ListaPageState extends State<ListaPage> {
         );
       },
     );
+  }
+
+  void _agregar10(){
+    for(var i=1; i<10; i++){
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+    }
+    setState(() {
+
+    });
   }
 }
