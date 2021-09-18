47058d4edb9d56e11ea3ec8ddd31ad95898f8356
Edd-war <digiespirit.1807@gmail.com>
2021-09-18 00:42:58 -0500

    Listview Builder
    En este commit se crea un nuevo recurso llamado listview_page.dart y aquí mismo construye una simple lista de imágenes tomadas por scraping para mostrarlas de forma estática

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,11 +1,12 @@
-import 'package:componentes/src/pages/animated_container.dart';
-import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
+import 'package:componentes/src/pages/listview_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/card_page.dart';
-import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 
 Map <String, WidgetBuilder> getApplicationRoutes() => <String, WidgetBuilder>{
@@ -15,7 +16,8 @@ Map <String, WidgetBuilder> getApplicationRoutes() => <String, WidgetBuilder>{
     'card'                  : (BuildContext context) => CardPage(),
     'animatedContainer'     : (BuildContext context) => AnimatedContainerPage(),
     'inputs'                : (BuildContext context) => InputPage(),
-    'slider'                  : (BuildContext context) => SliderPage(),
+    'slider'                : (BuildContext context) => SliderPage(),
+    'list'                  : (BuildContext context) => ListaPage(),
   };
 
 
