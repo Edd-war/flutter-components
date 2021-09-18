eb43464d67dda91ba5c37c5ccd2c59a7977c63d9
Edd-war <digiespirit.1807@gmail.com>
2021-09-18 00:01:42 -0500

    Slider
    Creación de nuevo recurso añadiendo la configuración tradicional vista en el curso.
    En el recurso creado llamado slider_page.dart se crea un slider que va relacionado a una imagen png que hace el efecto de modificar el tamaño de la imagen mediante los límites puestos en el slider.

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  const SliderPage({Key? key}) : super(key: key);
+
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+
+  double _valorSlider = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Slider'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlider(),
+            Expanded(
+                child: _cearImagen()
+            ),
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamaño de la imagen',
+      // divisions: 20,
+      value: 100,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor){
+        setState(()=> _valorSlider = valor);
+      }
+    );
+  }
+
+  Widget _cearImagen() {
+    return Image(
+      image: NetworkImage('https://i.pinimg.com/originals/f6/be/93/f6be93339f7cd625f2d183625621e3e1.png'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
