ee9be2640daa1c2fcedf09de98b96bf44abcb1c8
Edd-war <digiespirit.1807@gmail.com>
2021-09-17 21:52:36 -0500

    Email y Password fields
    Se crearon las cajas de texto para correo y contraseña, cada uno con su propio tipo de propiedad.

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -9,6 +9,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = "";
+  String _email  = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +21,12 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           _crearInput(),
           Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
           _crearPersona(),
+
         ],
       ),
     );
@@ -49,9 +55,44 @@ class _InputPageState extends State<InputPage> {
     );
   }
 
+
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: 'Email',
+          labelText: 'Email',
+          suffixIcon: Icon(Icons.alternate_email),
+          icon: Icon(Icons.email)
+      ),
+      onChanged: (valor) => setState(() => _email = valor),
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: 'Password',
+          labelText: 'Password',
+          suffixIcon: Icon(Icons.lock_open),
+          icon: Icon(Icons.lock)
+      ),
+      onChanged: (valor) => setState(() => _email = valor),
+    );
+  }
+
+
   Widget _crearPersona() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
   }
 }
